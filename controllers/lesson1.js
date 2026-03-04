320b19a3d004a944d09f5f0601d9d5f537beba9a
LaceySana <laceysanabria@gmail.com>
2026-03-03 20:44:58 -0700

    added controller and route

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const sayHello = (req, res) => {
+    res.send("Hello");
+}
+
+const displayMyName = (req, res) => {
+    res.send("Lacey Sanabria");
+}
+
+module.exports = {
+    sayHello,
+    displayMyName
+}
